38c65f05a0a9001e35fde52d85d4dc447b3a687d
Pasakorn Tiwatthanont <ptiwatthanont@gmail.com>
2024-08-10 12:14:44 +0700

    Init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/data-endpoint', (req, res) => {
+    const time = Date.now();
+    const value = Math.random() * 100;
+    res.json({time, value});
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
